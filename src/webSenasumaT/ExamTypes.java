e0b198d7b548c86aa4237e2c19fc8e43fa1ece66
rezghansa <rezghansa@gmail.com>
2016-08-29 11:00:21 +0530

    Exam Types Insert

--- src/webSenasumaT/ExamTypes.java
+++ src/webSenasumaT/ExamTypes.java
@@ -13,5 +13,29 @@ public class ExamTypes implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private int examTypeId = 0;
+	private String examTypeName;
+	public int getExamTypeId() {
+		return examTypeId;
+	}
+	public void setExamTypeId(int examTypeId) {
+		this.examTypeId = examTypeId;
+	}
+	public String getExamTypeName() {
+		return examTypeName;
+	}
+	public void setExamTypeName(String examTypeName) {
+		this.examTypeName = examTypeName;
+	}
+	
+	public void setExamType(){
+		 DbConnector.connectToDatabase();
+		 int autoTempId = DbConnector.getPrimaryKeyLastValue("examTypes", "examTypeId");
+		 examTypeId = autoTempId+1;
+		 String insertQuery = "insert into examTypes(examTypeId,examType) values("+examTypeId+",'"+examTypeName+"')";
+		 DbConnector.InsertionQuery(insertQuery);
+		 DbConnector.ClearConnection();
+	}
+	
 
 }
