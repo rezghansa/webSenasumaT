16edc8cab706137a50812dd5f9abff2f6c7b0316
rezghansa <rezghansa@gmail.com>
2016-09-01 15:32:59 +0530

    exam papaer generator

--- src/webSenasumaT/ExamPaperGenerator.java
+++ src/webSenasumaT/ExamPaperGenerator.java
@@ -1,5 +1,19 @@
 package webSenasumaT;
 
-public class ExamPaperGenerator {
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "ExamPaperGenerator", eager = true)
+@SessionScoped
+public class ExamPaperGenerator implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
 
 }
